bb382f3efa23b71136f4c76bfceb65605242c160
Parker Catalano <catalano.work@gmail.com>
2018-02-01 20:11:04 +0000

    automatically backed up by learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,2 +1,13 @@
 class SuperHero
+  attr_accessor :name, :power, :bio
+  HEROES = []
+
+  def initialize(args)
+    @name = args[:name]
+    HEROES << self
+  end
+
+  def self.all
+    HEROES
+  end
 end
