d3f0d09e9fef3eba54fe2c0bb78af9af153a3bba
Parker Catalano <catalano.work@gmail.com>
2018-02-01 20:24:57 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,8 @@ class App < Sinatra::Base
     post '/teams' do
       @team = Team.new(params["team"])
 
-      params["team"]["members"]
+      params["team"]["members"].each do |hero|
+        hero.name = params[:name]
       erb :team
     end
 
